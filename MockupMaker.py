145d716463409cb50f0761a1c5ed89cb81693bc6
Frank Flitton <frank@tdlab.io>
2018-11-20 01:20:32 -0500

    stack with masks

--- MockupMaker.py
+++ MockupMaker.py
@@ -1,4 +1,4 @@
-from os import listdir
+from os import listdir, path
 from os.path import isfile, join
 
 from moviepy.editor import *
@@ -6,7 +6,10 @@ from moviepy.editor import *
 
 input_folder = './_input/'
 output_folder = '_output/'
-
+center_xy = {
+    'pixel': (753,136),
+    'iphone': (753,136),
+}
 
 def overlayFootage(
     input=None,
@@ -14,6 +17,7 @@ def overlayFootage(
     device='pixel',
     color='white',
 ):
+    # Anayze clip that the composition will be based off of
     video_clip = VideoFileClip(
         input_folder + input
         ).fx(
@@ -21,49 +25,78 @@ def overlayFootage(
             height=821
         ).set_position(
             ('center', 'center')
+        ).on_color(
+            size=(1920,1080),
+            color=(0, 0, 0),
+            pos=center_xy[device],
+            col_opacity=1,
+        ).set_position(
+            ('center', 'center')
         )
+
+    # set local vars
     duration = video_clip.duration
 
     wallpaper_img = './img/wallpaper/' + wallpaper + '.jpg'
     device_img = './img/device/' + device.lower() + '/' + color.lower() + '.png'
-    mask_img = './img/device/' + device.lower() + '/' + 'mask.png   '
+    mask_img = './img/device/' + device.lower() + '/' + 'mask.png'
 
+    export_filename = output_folder
+    export_filename += path.splitext(input)[0]
+    export_filename += '-' + device.lower()
+    export_filename += '-' + color.lower()
+    export_filename += '.mp4'
+
+    # define clipping mask area
     mask_clip = ImageClip(
-        device_img,
+        mask_img,
         ismask=True,
     )
 
+    # Create video overlay
     wallpaper_clip = ImageClip(
-        img=wallpaper_img,
-        ismask=False,
-        transparent=True,
+        wallpaper_img
     ).set_duration(
         duration
-    ).set_mask(
-        mask_clip
+    ).set_position(
+        ('center', 'center')
     )
 
     device_clip = ImageClip(
         device_img
     ).set_duration(
         duration
-    ).set_mask(
+    ).set_position(
+        ('center', 'center')
+    )
+
+    video_clip = video_clip.set_mask(
         mask_clip
     )
 
+    # Stack video clips together
+    overlay_clip = CompositeVideoClip(
+        clips=[
+            wallpaper_clip,
+            device_clip,
+        ],
+    )
 
     video = CompositeVideoClip(
         [
-            video_clip,
+            # overlay_clip,
             wallpaper_clip,
             device_clip,
-        ]
+            video_clip,
+        ],
+        use_bgclip=True
     )
 
+    # Export video
     video.write_videofile(
-        "test.mp4",
+        export_filename,
         codec='mpeg4',
-        preset='slow',
+        preset='ultrafast',
     )
 
 def getInputVideos():
@@ -73,13 +106,9 @@ def getInputVideos():
     ]
     print(input_files)
 
-    # for file in input_files:
-    #     overlayFootage(
-    #         input=file
-    #     )
-    overlayFootage(
-        input=input_files[0]
-    )
+    for file in input_files:
+        overlayFootage(
+            input=file
+        )
 
-# clip = VideoFileClip("myHolidays.mp4")
 getInputVideos()
